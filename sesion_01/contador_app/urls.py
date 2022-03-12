1c0a069b015cd5c5bea074ca2b504b9272f87a1b
Mauricio Chávez Olea <30444800+ultr4nerd@users.noreply.github.com>
2022-03-12 15:04:46 -0600

    :sparkles: Parte 2 de la sesión 1

--- sesion_01/contador_app/urls.py
+++ sesion_01/contador_app/urls.py
@@ -2,10 +2,11 @@
 
 from django.urls import path
 
-from .views import hola_mundo
+from .views import hola_mundo, contador
 
 app_name = 'contador'
 
 urlpatterns = [
     path("hola_mundo/", hola_mundo),
+    path("contador/", contador),
 ]
--- sesion_01/contador_app/views.py
+++ sesion_01/contador_app/views.py
@@ -1,7 +1,9 @@
 """Contador app view"""
 
 import random
+from datetime import date
 
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
@@ -10,4 +12,23 @@ def hola_mundo(request):
         "mi_numero": random.randint(0, 10),
         "lista_compras": ["Zanahoria", "Leche", "Pastel"]
     }
-    return render(request, "index.html", context)
+    return render(request, "contador_app/index.html", context)
+
+
+def contador(request):
+    """Cuenta los días que faltan para navidad"""
+    now = date.today()
+    if now.month == 12 and now.day > 25:
+        christmas = date(now.year + 1, 12, 25)
+    else:
+        christmas = date(now.year, 12, 25)
+    days_until_christmas = (christmas - now).days
+
+    if days_until_christmas == 0:
+        message = "¡Ya es navidad!"
+    elif days_until_christmas == 1:
+        message = "Falta 1 día para navidad"
+    else:
+        message = f"Faltan {days_until_christmas} días para navidad"
+
+    return HttpResponse(message)
